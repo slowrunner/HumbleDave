682f54c9249f4465c473639b8e207915e5ca1a6f
slowrunner <slowrunner@users.noreply.github.com>
2023-11-09 09:36:14 -0500

    battery reading diff when charging

--- plib/battery.py
+++ plib/battery.py
@@ -10,7 +10,8 @@ import sys
 sys.path.insert(1,"/home/ubuntu/HumbleDave/plib/")
 from noinit_easygopigo3 import EasyGoPiGo3
 
-REV_PROTECT_DIODE = 0.81    # The GoPiGo3 has a reverse polarity protection diode drop of 0.6v to 0.8v (n=2)
+CHARGING_ADJUST_V = 0.37    # When charging the GoPiGo3 reading is closer to the actual battery voltage
+REV_PROTECT_DIODE = 0.9    # The GoPiGo3 has a reverse polarity protection diode drop of 0.6v to 0.8v (n=2)
                             # This value results in average readings vs battery voltage error of +/- 0.03
 SAFETY_SHUTDOWN_vBatt = 9.75   # Battery Discharge Protection Circuit allows down to 8.15v (9.75 leaves ~15m reserve)
 SAFETY_SHUTDOWN_vReading = SAFETY_SHUTDOWN_vBatt - REV_PROTECT_DIODE   # 8.5v EasyGoPiGo3.volt() reading
@@ -18,12 +19,17 @@ WARNING_LOW_vBatt = 10.0       # Give (~20 minutes) Advance Warning before safet
 
 def vBatt_vReading(egpg):
 	vReading = egpg.volt()
-	vBatt = vReading + REV_PROTECT_DIODE
+	if (vReading > 11.7):
+	  # charging
+	  vBatt = vReading + REV_PROTECT_DIODE - CHARGING_ADJUST_V
+	  # print("charging")
+	else:
+	  vBatt = vReading + REV_PROTECT_DIODE
 	return vBatt,vReading
 
 def voltages_string(egpg):
         vBatt, vReading = vBatt_vReading(egpg)
-        return "Current Battery {:.2f}v EasyGoPiGo3 Reading {:.2f}v".format(vBatt,vReading)
+        return "Current Battery {:.1f}v EasyGoPiGo3 Reading {:.2f}v".format(vBatt,vReading)
 
 def too_low(egpg):
 	vBatt, _ = vBatt_vReading(egpg)
