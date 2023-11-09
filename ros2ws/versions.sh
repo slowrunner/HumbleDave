8073f9d8c386ec1fad40e3ce35528874e6cc54e8
slowrunner <slowrunner@users.noreply.github.com>
2023-11-09 11:45:50 -0500

    adding ros2ws from ROS2-GoPiGo3

---
+++ ros2ws/versions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\n*** VERSIONS ***"
+
+echo "*** ROS 2 Humble Hawksbill ***"
+
+echo "*** Ubuntu 22.04 Server (64-bit)"
+
+echo "*** OpenCV 4.5.4"
+
+echo "*** Custom built libcamera"
+
+echo "*** YDLIDAR-SDK version 1.1.2"
+
+echo "*** ROS2-GoPiGo3 image: v4 Updates applied Nov 2023"
+
+echo "*** Python3.10 ***"
+
