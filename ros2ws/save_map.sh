15e5764982d32ca6d7197c247f6c754eefce8773
slowrunner <slowrunner@users.noreply.github.com>
2023-11-13 12:29:58 -0500

    combined save map and export jpg

--- ros2ws/save_map.sh
+++ ros2ws/save_map.sh
@@ -3,3 +3,10 @@
 echo -e "\n*** SAVING MAP TO xyzzy.map"
 echo '*** ros2 run nav2_map_server map_saver_cli -f "xyzzy.map" '
 ros2 run nav2_map_server map_saver_cli -f "xyzzy.map"
+echo -e "\n*** WAITING 10s FOR MAP SAVER"
+sleep 10
+# echo -e "\n*** NOW RUN export_jpg_map.sh ***"
+echo -e "\n*** Exporting xyzzy.map.pgm to jpg format: xyzzy.map.jpg"
+echo "***  mogrify -format jpg xyzzy.map.pgm"
+mogrify -format jpg xyzzy.map.pgm
+echo "*** xyzzy.map.jpg created"
